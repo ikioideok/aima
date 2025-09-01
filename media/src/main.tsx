91a44017073b4ba0254d51a53c630df960da317d
ikioideok <said99mcx@gmail.com>
2025-09-01 16:44:35 +0900

    media: Scroll to top on route change to avoid mid-page position when navigating to articles

--- media/src/main.tsx
+++ media/src/main.tsx
@@ -1,12 +1,23 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import App from "./App.tsx";
 import ArticlePage from "./pages/ArticlePage.tsx";
 import Admin from "./pages/Admin.tsx";
 import "./styles/globals.css";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    // Ensure navigating to a new route resets scroll to top
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter basename="/media">
+    <ScrollToTop />
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/articles/:slug" element={<ArticlePage />} />
